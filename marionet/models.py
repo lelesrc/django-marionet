477d76bd8df202d2b24961a0f497da5c73cd0bef
lamikae <mikael.lammentausta+github+@gmail.com>
2010-03-11 06:27:27 +0200

    Models copied from django-portlets (BSD license) author: diefenbach

--- marionet/models.py
+++ marionet/models.py
@@ -1,11 +1,43 @@
 # -*- coding: utf-8 -*-
+#
+# Models copied from django-portlets (BSD license)
+# author: diefenbach
+#
 
 # django imports
 from django import forms
-from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes import generic
 from django.db import models
 from django.template.loader import render_to_string
 from django.utils.translation import ugettext_lazy as _
 
+from portlets.models import Portlet
 
+class TextPortlet(Portlet):
+    """A simple portlet to display some text.
+    """
+    text = models.TextField(_(u"Text"), blank=True)
+
+    def __unicode__(self):
+        return "%s" % self.id
+
+    def render(self, context=None):
+        """Renders the portlet as html.
+        """
+        return render_to_string("portlets/text_portlet.html", {
+            "title" : self.title,
+            "text" : self.text
+        })
+
+    def form(self, **kwargs):
+        """
+        """
+        return TextPortletForm(instance=self, **kwargs)
+
+class TextPortletForm(forms.ModelForm):
+    """Form for the TextPortlet.
+    """
+    class Meta:
+        model = TextPortlet
+        
+from portlets.utils import register_portlet
+register_portlet(TextPortlet, "TextPortlet")
--- test/portlet_test.py
+++ test/portlet_test.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+#
+# Test cases copied from django-portlets (BSD license)
+# author: diefenbach
+#
+
 import unittest
 
 # django imports
@@ -18,5 +23,62 @@ class PortletTest(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_foo(self):
-        pass
+    def test_slot(self):
+        """
+        """
+        slot = Slot.objects.create(name="Left")
+        self.assertEqual(slot.name, "Left")
+
+    def test_portlet_registration(self):
+        """
+        """
+        portlet_registration = PortletRegistration.objects.create(
+            type = "textportlet",
+            name = "TextPortlet",
+        )
+
+        self.assertEqual(portlet_registration.type, "textportlet")
+        self.assertEqual(portlet_registration.name, "TextPortlet")
+        self.assertEqual(portlet_registration.active, True)
+
+        # try to add another portlet with same type or name
+        self.assertRaises(IntegrityError, PortletRegistration.objects.create, type = "textportlet")
+        self.assertRaises(IntegrityError, PortletRegistration.objects.create, name = "TextPortlet")
+
+        # add another portlet with other name
+        portlet_registration_2 = PortletRegistration.objects.create(
+            type = "textportlet_2",
+            name = "TextPortlet_2",
+        )
+
+    def test_portlet_assignment(self):
+        """
+        """
+        slot = Slot.objects.create(name="Left")
+        page = FlatPage.objects.create(url="/test/", title="Test")
+        portlet = TextPortlet.objects.create(title="Text")
+
+        portlet_assignment = PortletAssignment.objects.create(
+            slot = slot,
+            content = page,
+            portlet = portlet,
+        )
+
+        self.assertEqual(portlet_assignment.slot, slot)
+        self.assertEqual(portlet_assignment.content, page)
+        self.assertEqual(portlet_assignment.portlet, portlet)
+
+    def test_portlet_blocking(self):
+        """
+        """
+        slot = Slot.objects.create(name="Left")
+        page = FlatPage.objects.create(url="/test/", title="Test")
+
+        portlet_blocking = PortletBlocking.objects.create(
+            slot = slot,
+            content = page,
+        )
+
+        self.assertEqual(portlet_blocking.slot, slot)
+        self.assertEqual(portlet_blocking.content, page)
+
