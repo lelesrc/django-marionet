414daa0d690763a0640441d3d6235a216cd8833e
lamikae <mikael.lammentausta+github+@gmail.com>
2010-03-29 02:19:55 +0300

    first templatetags

---
+++ marionet/templatetags/marionet_tags.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#
+
+from django import template
+
+from portlets.models import Portlet
+from portlets.utils import register_portlet
+
+from marionet import log
+
+register = template.Library()
+
+@register.inclusion_tag('marionet/portlet.html', takes_context=True)
+def marionet(context,portlet):
+    """
+    """
+    log.debug(context)
+    return {
+        'portlet': context['portlet']
+    }
